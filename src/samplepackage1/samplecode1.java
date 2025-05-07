caae37fdb286fb61b39e0d4822c249ee1a43a209
Ashutosh Choubey <ashutoshchoubey16@gmail.com>
2025-05-07 23:32:25 +0530

    change

--- src/samplepackage1/samplecode1.java
+++ src/samplepackage1/samplecode1.java
@@ -1,4 +1,7 @@
 package samplepackage1;
 
 public class samplecode1 {
+    public static void main(String[] args) {
+        System.out.println("Hello myself Ashutosh! I am attending Java classes");
+    }
 }
